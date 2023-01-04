82a8d0c3a07428f16f4a64589e50d1edf5fdc943
Nedim89 <nedim.custovic@gmail.com>
2023-01-04 11:40:03 +0000

    Comit 1.2

--- birthday-project/src/App.tsx
+++ birthday-project/src/App.tsx
@@ -4,14 +4,14 @@ import data from './data'
 import List from './List'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [people, setPeople] = useState(data);
 
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
-       <List /> 
-
+        <h3>{people.length} birthdays today</h3>
+       <List people={people}/> 
+        <button onClick={()=> console.log('You clicked me!')}>Clear all!</button>
       </section>
     </main>
   )
